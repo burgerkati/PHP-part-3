3827ae1e9ee52019e3614ee517c43941b915c2f3
katherineburgain <68737754+burgerkati@users.noreply.github.com>
2021-06-10 12:19:11 +0200

    change exercise 2 to add example with use of default parameter in function

--- exercice_2/index.php
+++ exercice_2/index.php
@@ -1,6 +1,14 @@
 <?php
+/*
+first example
 function showMeString() {
   echo 'Hallo Leute!';
 }
 showMeString();
+*/
+// second example with default parameter in function
+function whatDayIsIt($day = 'Mittwoch.') {
+  return "Es is $day";
+}
+echo whatDayIsIt();
 ?>
